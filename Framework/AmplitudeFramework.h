9165657cde63747e918d00556dc86e2d5be3c0bf
Pete Schwamb <pete@schwamb.net>
2022-04-03 12:42:05 -0500

    Fix compiler warnings

--- Framework/AmplitudeFramework.h
+++ Framework/AmplitudeFramework.h
@@ -4,7 +4,4 @@
 #import <Amplitude/AMPRevenue.h>
 #import <Amplitude/AMPTrackingOptions.h>
 #import <Amplitude/Amplitude+SSLPinning.h>
-
-#if TARGET_OS_WATCH
 #import <Amplitude/AMPBackgroundNotifier.h>
-#endif
